a083a1571b7a2937f44a2e3441469be0d61074d5
Luis Duenas <luis.alonso.duenas.m@gmail.com>
2017-05-28 22:56:50 -0700

    upload project

---
+++ Gif-Search/Gif_Search/Services/API_Conection.cs
@@ -0,0 +1,38 @@
+﻿
+
+using Gif_Search.Models;
+using Gif_Search.ViewModels;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static Gif_Search.Models.RootItem;
+
+namespace Gif_Search.Services
+{
+    public class API_Conection : BaseViewModel
+    {
+        public async Task<ObservableCollection<Item>> getApiResults(string searchText)
+        {
+            var uri = new Uri("http://api.giphy.com/v1/gifs/search?q=" + searchText + "&api_key=dc6zaTOxFJmzC&limit=100&lang=es");
+            Rootobject root;
+            string results = string.Empty;
+            ObservableCollection<Item> items = new ObservableCollection<Item>();
+            HttpClient wc = new HttpClient();
+            HttpResponseMessage response = await wc.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            dynamic jo = JObject.Parse(responseBody);
+            root = jo.ToObject<Rootobject>();
+
+            foreach (var data in root.data)
+            {
+                items.Add(new Item { Id = Guid.NewGuid().ToString(), Text = data.slug, Description = data.username, Source = data.images.original.url });
+            }
+            return items;
+        }
+    }
+}
---
+++ Gif-Search/Gif_Search/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Gif_Search.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
